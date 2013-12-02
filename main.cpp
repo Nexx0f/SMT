fc7c80681c0fdcc4e42bd48b84ed880e15e93de8
Nexx0f <nexxof@yandex.ru>
2013-12-02 21:34:44 +0400

    Added test file to repository and added small calling interface

--- main.cpp
+++ main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include "Tokeniser.h"
 #include "Translator.h"
+#include <string.h>
 
-int main(int argc, char **argv) 
+int main(int argc, char* argv[]) 
 {
     Tokeniser t;
-    t.SetTokeniser ("lighter_test.txt", false);
+    bool dump = false;
+    if (argc >= 4)
+        if (!strcmp (argv[3], "init_dump")) dump = true;
+    
+    if (argc >= 2) t.SetTokeniser (argv[1], dump);
+    else           t.SetTokeniser ("input.txt", dump);
     t.Tokenise ();
     
     Translator translator;
-    translator.SetTranslator (&t, true);
+    translator.SetTranslator (&t, dump);
     
     translator.Translate ();
     
     Generator gen (translator.states.size(),
                    translator.inputs.size(),
                    translator.outputs.size(),
-                   true,
+                   dump,
                    translator.conditionalTransits,
                    translator.conditionalOutputs);
     
-    FILE* output = fopen ("output.v", "w");
+    FILE* output;
+    if (argc >= 3) output = fopen (argv[2], "w");
+    else           output = fopen ("output.v", "w");
     gen.Generate (output);
     fclose (output);
 }
